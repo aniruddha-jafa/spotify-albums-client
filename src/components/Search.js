7b140d5b360dfc0a2e39b5e0f724122003592bef
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-08-02 16:13:57 +0530

    Defined Search component

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+function Search() {
+  return (
+    <>
+      <form action="">
+        <input
+          type="text"
+          name="movieName"
+          placeholder="Search an artist..."
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default Search
