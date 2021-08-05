94bc0ce471d501b5d28cb7d0d32467e94b4e53bb
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-08-05 13:01:30 +0530

    Added mock data for 'album' & 'track'

--- src/components/album/albumsMockData.js
+++ src/components/album/albumsMockData.js
@@ -1,3 +1,5 @@
+
+// Coldplay, 2014 onwards
 export const ALBUMS_SAMPLE_JSON = [
   {
     "album_group": "album",
@@ -396,3 +398,413 @@ export const ALBUMS_SAMPLE_JSON = [
     "uri": "spotify:album:2G4AUqfwxcV1UdQjm2ouYr"
   }
 ]
+
+// Ghost Stories, 2014
+export const ALBUM_SAMPLE_JSON = {
+  "album_type": "album",
+  "artists": [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+      },
+      "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+      "id": "4gzpq5DPGxSnKTe4SA8HAU",
+      "name": "Coldplay",
+      "type": "artist",
+      "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+    }
+  ],
+  "copyrights": [
+    {
+      "text": "© 2014 Parlophone Records Limited, a Warner Music Group Company.",
+      "type": "C"
+    },
+    {
+      "text": "℗ 2014 Parlophone Records Limited, a Warner Music Group Company.",
+      "type": "P"
+    }
+  ],
+  "external_ids": {
+    "upc": "825646216284"
+  },
+  "external_urls": {
+    "spotify": "https://open.spotify.com/album/2G4AUqfwxcV1UdQjm2ouYr"
+  },
+  "genres": [],
+  "href": "https://api.spotify.com/v1/albums/2G4AUqfwxcV1UdQjm2ouYr",
+  "id": "2G4AUqfwxcV1UdQjm2ouYr",
+  "images": [
+    {
+      "height": 640,
+      "url": "https://i.scdn.co/image/ab67616d0000b273f864bcdcc245f06831d17ae0",
+      "width": 640
+    },
+    {
+      "height": 300,
+      "url": "https://i.scdn.co/image/ab67616d00001e02f864bcdcc245f06831d17ae0",
+      "width": 300
+    },
+    {
+      "height": 64,
+      "url": "https://i.scdn.co/image/ab67616d00004851f864bcdcc245f06831d17ae0",
+      "width": 64
+    }
+  ],
+  "label": "Parlophone UK",
+  "name": "Ghost Stories",
+  "popularity": 72,
+  "release_date": "2014-05-19",
+  "release_date_precision": "day",
+  "total_tracks": 9,
+  "tracks": {
+    "href": "https://api.spotify.com/v1/albums/2G4AUqfwxcV1UdQjm2ouYr/tracks?offset=0&limit=50&market=ES",
+    "items": [
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 216626,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/0FMjqbY3aWo1QDbo3GwXib"
+        },
+        "href": "https://api.spotify.com/v1/tracks/0FMjqbY3aWo1QDbo3GwXib",
+        "id": "0FMjqbY3aWo1QDbo3GwXib",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Always in My Head",
+        "preview_url": "https://p.scdn.co/mp3-preview/d4f7b43d4cb96d09d09c778959cb26102475a58c?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 1,
+        "type": "track",
+        "uri": "spotify:track:0FMjqbY3aWo1QDbo3GwXib"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 285014,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/23khhseCLQqVMCIT1WMAns"
+        },
+        "href": "https://api.spotify.com/v1/tracks/23khhseCLQqVMCIT1WMAns",
+        "id": "23khhseCLQqVMCIT1WMAns",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Magic",
+        "preview_url": "https://p.scdn.co/mp3-preview/60ec5dcb39393978dd136c8cdf517f6a834ac620?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 2,
+        "type": "track",
+        "uri": "spotify:track:23khhseCLQqVMCIT1WMAns"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 228373,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/6c6W25YoDGjTq3qSPOga5t"
+        },
+        "href": "https://api.spotify.com/v1/tracks/6c6W25YoDGjTq3qSPOga5t",
+        "id": "6c6W25YoDGjTq3qSPOga5t",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Ink",
+        "preview_url": "https://p.scdn.co/mp3-preview/ddf9fc3519894ae765f904d9e03b3a14281c8872?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 3,
+        "type": "track",
+        "uri": "spotify:track:6c6W25YoDGjTq3qSPOga5t"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 246000,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/0fQuzbQNLcD1ofo7B2NcFI"
+        },
+        "href": "https://api.spotify.com/v1/tracks/0fQuzbQNLcD1ofo7B2NcFI",
+        "id": "0fQuzbQNLcD1ofo7B2NcFI",
+        "is_local": false,
+        "is_playable": true,
+        "name": "True Love",
+        "preview_url": "https://p.scdn.co/mp3-preview/4a3d85765fe1547ece26976d90b987a7ece71c44?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 4,
+        "type": "track",
+        "uri": "spotify:track:0fQuzbQNLcD1ofo7B2NcFI"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 294666,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/4GKk1uNzpxIptBuaY97Dkj"
+        },
+        "href": "https://api.spotify.com/v1/tracks/4GKk1uNzpxIptBuaY97Dkj",
+        "id": "4GKk1uNzpxIptBuaY97Dkj",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Midnight",
+        "preview_url": "https://p.scdn.co/mp3-preview/eb56def641f2ed627d7c93e913516e531ab2b20a?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 5,
+        "type": "track",
+        "uri": "spotify:track:4GKk1uNzpxIptBuaY97Dkj"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 234413,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/22uzF19LxQW87kOVkR79Fq"
+        },
+        "href": "https://api.spotify.com/v1/tracks/22uzF19LxQW87kOVkR79Fq",
+        "id": "22uzF19LxQW87kOVkR79Fq",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Another's Arms",
+        "preview_url": "https://p.scdn.co/mp3-preview/b7fb9a637b20e480add92515b6b765c452bddcd5?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 6,
+        "type": "track",
+        "uri": "spotify:track:22uzF19LxQW87kOVkR79Fq"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 321680,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/2u4uhtETtcULnkBfFHSnDX"
+        },
+        "href": "https://api.spotify.com/v1/tracks/2u4uhtETtcULnkBfFHSnDX",
+        "id": "2u4uhtETtcULnkBfFHSnDX",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Oceans",
+        "preview_url": "https://p.scdn.co/mp3-preview/5ce7238c6b2335556f552c7e6ba6253e82e3530f?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 7,
+        "type": "track",
+        "uri": "spotify:track:2u4uhtETtcULnkBfFHSnDX"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 267866,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/0FDzzruyVECATHXKHFs9eJ"
+        },
+        "href": "https://api.spotify.com/v1/tracks/0FDzzruyVECATHXKHFs9eJ",
+        "id": "0FDzzruyVECATHXKHFs9eJ",
+        "is_local": false,
+        "is_playable": true,
+        "name": "A Sky Full of Stars",
+        "preview_url": "https://p.scdn.co/mp3-preview/c078ecffbf466b8521e135766dd4f36699846feb?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 8,
+        "type": "track",
+        "uri": "spotify:track:0FDzzruyVECATHXKHFs9eJ"
+      },
+      {
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+            },
+            "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+            "id": "4gzpq5DPGxSnKTe4SA8HAU",
+            "name": "Coldplay",
+            "type": "artist",
+            "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 323480,
+        "explicit": false,
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/77yuzxCS3csrgTPSW0pvyk"
+        },
+        "href": "https://api.spotify.com/v1/tracks/77yuzxCS3csrgTPSW0pvyk",
+        "id": "77yuzxCS3csrgTPSW0pvyk",
+        "is_local": false,
+        "is_playable": true,
+        "name": "O",
+        "preview_url": "https://p.scdn.co/mp3-preview/227979f9d5f8b9820b6b3b522b8eb3b9e8322b21?cid=3fedd90b44d0457f95d1224ac7c96f71",
+        "track_number": 9,
+        "type": "track",
+        "uri": "spotify:track:77yuzxCS3csrgTPSW0pvyk"
+      }
+    ],
+    "limit": 50,
+    "next": null,
+    "offset": 0,
+    "previous": null,
+    "total": 9
+  },
+  "type": "album",
+  "uri": "spotify:album:2G4AUqfwxcV1UdQjm2ouYr"
+}
+
+// 'Magic', from Ghost Stories, 2014
+export const TRACK_SAMPLE_JSON = {
+  "album": {
+    "album_type": "album",
+    "artists": [
+      {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+        },
+        "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+        "id": "4gzpq5DPGxSnKTe4SA8HAU",
+        "name": "Coldplay",
+        "type": "artist",
+        "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+      }
+    ],
+    "external_urls": {
+      "spotify": "https://open.spotify.com/album/2G4AUqfwxcV1UdQjm2ouYr"
+    },
+    "href": "https://api.spotify.com/v1/albums/2G4AUqfwxcV1UdQjm2ouYr",
+    "id": "2G4AUqfwxcV1UdQjm2ouYr",
+    "images": [
+      {
+        "height": 640,
+        "url": "https://i.scdn.co/image/ab67616d0000b273f864bcdcc245f06831d17ae0",
+        "width": 640
+      },
+      {
+        "height": 300,
+        "url": "https://i.scdn.co/image/ab67616d00001e02f864bcdcc245f06831d17ae0",
+        "width": 300
+      },
+      {
+        "height": 64,
+        "url": "https://i.scdn.co/image/ab67616d00004851f864bcdcc245f06831d17ae0",
+        "width": 64
+      }
+    ],
+    "name": "Ghost Stories",
+    "release_date": "2014-05-19",
+    "release_date_precision": "day",
+    "total_tracks": 9,
+    "type": "album",
+    "uri": "spotify:album:2G4AUqfwxcV1UdQjm2ouYr"
+  },
+  "artists": [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU"
+      },
+      "href": "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU",
+      "id": "4gzpq5DPGxSnKTe4SA8HAU",
+      "name": "Coldplay",
+      "type": "artist",
+      "uri": "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+    }
+  ],
+  "disc_number": 1,
+  "duration_ms": 285014,
+  "explicit": false,
+  "external_ids": {
+    "isrc": "GBAYE1400206"
+  },
+  "external_urls": {
+    "spotify": "https://open.spotify.com/track/23khhseCLQqVMCIT1WMAns"
+  },
+  "href": "https://api.spotify.com/v1/tracks/23khhseCLQqVMCIT1WMAns",
+  "id": "23khhseCLQqVMCIT1WMAns",
+  "is_local": false,
+  "is_playable": true,
+  "name": "Magic",
+  "popularity": 72,
+  "preview_url": "https://p.scdn.co/mp3-preview/60ec5dcb39393978dd136c8cdf517f6a834ac620?cid=3fedd90b44d0457f95d1224ac7c96f71",
+  "track_number": 2,
+  "type": "track",
+  "uri": "spotify:track:23khhseCLQqVMCIT1WMAns"
+}
