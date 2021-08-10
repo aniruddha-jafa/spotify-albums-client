37a69b2a1ec08da722230f0be4c12f2b9f306cb0
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2021-08-10 13:08:30 +0530

    Removed uneeded Home component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 import Navbar from './components/Navbar'
-import { Home, About } from './components/pages'
+import { About } from './components/pages'
 import Album from './components/album/Album'
+import Albums from './components/album/Albums'
 
-import { getAlbum } from './utils'
+import { getAlbum } from './components/album/utils'
+import Search from './components/Search'
+import { ALBUMS_SAMPLE_JSON } from './components/album/albumsMockData'
 
 function App() {
   const [album, setAlbum] = useState([])
@@ -15,7 +18,10 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
-            <Route exact path='/' component={Home}></Route>
+            <Route exact path='/'>
+              <Search />
+              <Albums albums={ALBUMS_SAMPLE_JSON}/>
+            </Route>
             <Route path='/about' component={About}></Route>
             <Route exact path='/album/:id' render={props => (
                 <Album {...props} 
--- src/components/pages.js
+++ src/components/pages.js
@@ -1,16 +1,3 @@
-import Search from './Search'
-import Albums from './album/Albums'
-
-import { ALBUMS_SAMPLE_JSON } from './album/albumsMockData'
-
-export function Home() {
-  return (
-    <>
-      <Search />
-      <Albums albums={ALBUMS_SAMPLE_JSON} />
-    </>
-  )
-}
 
 export function About() {
   const message = "This app uses Spotify's api to search for and display an artist's albums"
